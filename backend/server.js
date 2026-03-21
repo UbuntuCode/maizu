555fc0e5983f968eaf62d8295231be7d0012853e
sinethembamatsengwane73-dev <sinethembamatsengwane73@gmail.com>
2026-03-21 02:50:57 +0200

    Set up Express server with basic route

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
+const express = require("express")
 
+const app = express()
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+res.send("MAIZU backend is running 🚀")
+})
+
+const PORT = 5000
+
+app.listen(PORT, () => {
+console.log("Server running on port 5000")
+})
